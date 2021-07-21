24b7d47ccdff8661b8f3b8fb5904f900fb50f590
ekocahyanto007@gmail.com <ekocahyanto007@gmail.com>
2021-07-21 12:18:58 -0400

    Upload Tugas

---
+++ src/App.js
@@ -0,0 +1,202 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div id="header">
+          <span class="logo">
+            <a href="index.html">Yanto.web</a>
+          </span>
+          <span class="website-menu">
+            <a href="/template/frontend/index.html">Home</a>
+            <a href="/template/frontend/about.html">About</a>
+            <a href="/template/frontend/login.html">Login</a>
+          </span>
+        </div>
+        <div class="container">
+          <div id="konten">
+            <h1>Contoh Teks Artikel dan Strukturnya</h1>
+            <img
+              src="https://asset.kompas.com/crops/1BWHOrYAdSQePvLkouQ3S-hXAXE=/50x34:483x322/750x500/data/photo/2020/06/24/5ef2e96f255bc.jpg"
+              alt=""
+            />
+            <small>
+              <i>
+                Ilustrasi teks eksposisi(
+                <a href="shutterstock.com">shutterstock.com </a>)
+              </i>
+            </small>
+            <p>
+              <b>
+                <i>KOMPAS.com</i>
+              </b>{" "}
+              - Artikel merupakan salah satu jenis karya tulis. Teks artikel ada
+              yang berisikan persuasi atau berbagai informasi tambahan untuk
+              pembaca.
+            </p>
+            <p>
+              Teks artikel biasanya ditulis secara singkat, padat dan jelas agar
+              pembaca lebih mudah mengerti. Tidak hanya itu, biasanya dalam
+              artikel ada opini yang turut didukung dengan fakta.
+            </p>
+            <p>
+              Selain itu, artikel juga bersifat faktual dan informatif. Artinya
+              sesuai dengan fakta di lapangan serta untuk memberi informasi atau
+              pengetahuan kepada pembaca.
+            </p>
+            <p>
+              Mengutip dari Buku Cepat Menguasai Soal Bahasa Indonesia SMA dan
+              MA (2019) karya Tomi Rianto, teks artikel memiliki tiga struktur,
+              yaitu:
+            </p>
+            <ol>
+              <li>Tesis</li>
+              <p>
+                Tesis adalah gagasan utama dari permasalahan yang akan dibahas.
+                Tesis berangkat dari fakta yang ada.
+              </p>
+              <li>Argumentasi</li>
+              <p>
+                Argumentasi adalah pendapat yang disampaikan penulis yang juga
+                harus disesuaikan dengan fakta.
+              </p>
+              <li>Penegasan Ulang</li>
+              <p>
+                Penegasan ulang Penegasan ulang adalah bentuk penjelasan ulang
+                untuk memperkuat argumentasi yang dibuat penulis dan harus
+                sesuai dengan fakta.
+              </p>
+            </ol>
+            <p>Bagaimana contoh teks artikel beserta strukturnya?</p>
+            <p>
+              <i>Contoh:</i>
+            </p>
+            <p>
+              Pentingnya melakukan Peregangan Sebelum Olahraga Peregangan
+              sebelum berolahraga sangatlah penting. Tujuannya untuk menghindari
+              cedera serius dan ketegangan pada tubuh.
+            </p>
+            <p>
+              Melakukan peregangan sebelum berolahraga memang memiliki banyak
+              manfaat yang sangat baik untuk kesehatan tubuh. Para ahli
+              menyarankan jika sebaiknya peregangan dilakukan saat pagi hari,
+              tepatnya sebelum memulai aktivitas fisik. Bentuk peregangan yang
+              paling mudah dan menyehatkan adalah berlari. Para ahli mengatakan
+              jika berlari membawa dampak positif bagi orang yang melakukannya.
+              Berlari dapat melatih kekuatan dan kesehatan otot jantung.
+            </p>
+            <p>
+              Bentuk peregangan mudah lainnya adalah dengan memutar kepala,
+              tangan, pundak dan kaki searah jarum jam. Peregangan ini sangat
+              mudah untuk dilakukan di mana saja tanpa perlu mengeluarkan biaya
+              untuk melakukannya.
+            </p>
+            <p>
+              Contoh bentuk peregangan lainnya adalah dengan melakukan squat.
+              Squat dapat melatih banyak fungsi tubuh dalam satu gerakan serta
+              melatih kekuatan sendi serta ligamen dalam tubuh.
+            </p>
+            <p>
+              Maka disarankan untuk tidak lupa melakukan peregangan sebelum
+              berolahraga agar tubuh tetap sehat dan tidak mengalami cedera.
+            </p>
+            <p>
+              <b>Tesis:</b> Dalam contoh di atas, tesis berada di paragraf
+              pertama karena gagasan utama dari permasalahan yang dibahas adalah
+              dampak negatif jika tidak melakukan peregangan.
+            </p>
+            <p>
+              <b>Argumentasi:</b> Dalam contoh di atas, argumentasi berada di
+              paragraf ketiga karena penulis menyampaikan pendapat jika berlari
+              merupakan olahraga yang paling mudah dan menyehatkan. Pendapat
+              dari penulis ini dibuktikan dengan perkataan dari para ahli.
+            </p>
+            <p>
+              <b>Penegasan ulang:</b> Dalam contoh di atas, penegasan ulang
+              berada di paragraf terakhir karena penulis ingin mengingatkan agar
+              pembaca melakukan pemanasan sebelum berolahraga.
+            </p>
+            <table border="1">
+              <tbody>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Status</th>
+              </tbody>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Eko</td>
+                  <td>Active</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Yanto</td>
+                  <td>Active</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Yadi</td>
+                  <td>Suspend</td>
+                </tr>
+              </tbody>
+            </table>
+            <small>
+              <i>Kompas.com - 28/01/2021, 14:05 WIB</i>
+            </small>
+            <h2>
+              Bagikan :<i class="fa fa-facebook-square" aria-hidden="true"></i>
+              <i class="fa fa-twitter-square" aria-hidden="true"></i>
+              <i class="fa fa-whatsapp" aria-hidden="true"></i>
+            </h2>
+
+            <p>
+              <i>
+                Sumber artikel :
+                <a href="https://www.kompas.com/skola/read/2021/01/28/140503769/contoh-teks-artikel-dan-strukturnya?page=all">
+                  kompas.com
+                </a>
+              </i>
+            </p>
+          </div>
+
+          <div class="right">
+            <div id="about">
+              <h1>About Me</h1>
+              <img src="./img-about.jpg" alt="" />
+              <p>
+                "Jika kamu benar-benar menginginkan sesuatu, lambat laun kamu
+                pasti akan segera menemukan caranya."
+              </p>
+            </div>
+
+            <div id="follow">
+              <h1>Follow Me</h1>
+              <a href="https://www.facebook.com/exo.cahyanto">
+                <i class="fa fa-facebook-square" aria-hidden="true"></i>
+              </a>
+              <a href="https://mobile.twitter.com/Bang_ECa">
+                <i class="fa fa-twitter-square" aria-hidden="true"></i>
+              </a>
+              <a href="https://www.instagram.com/e_ca93/">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div id="footer">
+          <p>
+            © 2020 |
+            <a href="https://belajarbarengyanto.wordpress.com/">
+              belajarbarengyanto.wordpress.com
+            </a>
+            | Eko Cahyanto
+          </p>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
